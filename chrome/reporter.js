431398e30eb862158d1071160195fc8c96196ab2
Alex Conner <aconner@igodigital.com>
2012-08-31 16:33:46 -0400

    Report data in header, because IDK, JS hates me.

--- chrome/reporter.js
+++ chrome/reporter.js
@@ -2,7 +2,8 @@ var ajax = new XMLHttpRequest()
 var report = function(){
 	var latency = window.performance.timing.responseEnd - window.performance.timing.fetchStart
 	ajax.open("POST", "http://localhost:3000/", true)
-	ajax.send("latency=" + latency)
+	ajax.setRequestHeader("Latency", latency)
+	ajax.send(['latency='+latency])
 }
 
 window.document.addEventListener("load", report)
